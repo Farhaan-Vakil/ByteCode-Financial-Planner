d28c415777f84b8065737c27f8846c6e777a1ace
Zhi <ricknick005@gmail.com>
2025-08-06 23:43:14 -0400

    added made html for market news and got the api done for individual stock news

---
+++ app/public/news.js
@@ -0,0 +1,31 @@
+let articles = document.getElementById("articles");
+
+fetch(`/news`).then((response) => {
+    response.json().then((body) => {
+        for (i = 0; i < body.length; i ++) {
+            let article = document.createElement("article");
+            let headline = document.createElement("h2");
+            let a = document.createElement("a");
+            a.href = body[i].url;
+            a.textContent = (body[i].headline);
+            headline.appendChild(a);
+
+            let source = document.createElement("h3");
+            source.textContent = body[i].source;
+            let sum = document.createElement("p");
+            sum.textContent = body[i].summary;
+            let img = document.createElement("img");
+            img.src = body[i].image;
+            img.alt = body[i].source;
+
+            article.appendChild(headline);
+            article.appendChild(img);
+            article.appendChild(source);
+            article.appendChild(sum);
+
+            articles.appendChild(article);
+        }
+
+    });
+
+});
---
+++ app/zhiApi.js
@@ -0,0 +1,44 @@
+let express = require("express");
+let app = express();
+let hostname = "localhost";
+let port = 3000;
+let finnhub = require("finnhub");
+let env = require("../env.json");
+
+const finnhubClient = new finnhub.DefaultApi(env.finnkey);
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get(`/news`, (req, res) => {
+    finnhubClient.marketNews("general", {}, (error, data, response) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json({error: "Failed to fecth news"});
+
+        }
+        res.status(200).json(data);
+        console.log(data);
+    });
+})
+
+app.get(`/stockNews`, (req,res) => {
+    let stock = req.query.stock;
+    console.log(stock);
+    finnhubClient.companyNews(stock, "2025-07-01", "2025-08-01", (error,data,response) => {
+        
+        if (error) {
+            console.log(error);
+            res.status(500).json({error: "failed to fetch stock news"});
+        }
+        res.status(200).json(data);
+        console.log(data);
+
+
+    });
+})
+
+
+app.listen(port, hostname, function () {
+  console.log(`http://${hostname}:${port}`);
+});
