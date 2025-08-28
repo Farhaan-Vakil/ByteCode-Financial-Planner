d0136327591fb94cda2e81e3e87a73f50bb0cac9
FarhaanVakil <farhaanvakil2023@gmail.com>
2025-08-28 14:25:10 -0400

    Edited Production Build

--- app/server.js
+++ app/server.js
@@ -26,7 +26,7 @@ const yahooFinance = require('yahoo-finance2').default;
 
 const app = express();
 const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(session({
   secret: "supersecretkey",        
